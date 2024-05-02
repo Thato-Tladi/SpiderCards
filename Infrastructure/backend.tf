cdf45981d5695210e07f840d81b2155b15ecc83c
Thato Tladi <thato.tladi@bbd.co.za>
2024-05-02 02:14:52 +0200

    Cleanup

--- Infrastructure/backend.tf
+++ Infrastructure/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket = "spidercards-levelup-bucket"
-    key = "spidercards/terraform.tfstate"  # Specify the path/key for your state file
+    key = "spidercards/terraform.tfstate"
     region = "eu-west-1"
   }
 }

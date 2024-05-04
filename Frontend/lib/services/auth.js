88d598d3441fd18197040410c89f039b8bee851c
Thapelo Thoka <Thapelo.Thoka@bbd.co.za>
2024-05-04 22:09:45 +0200

    latest changes

---
+++ Frontend/lib/services/auth.js
@@ -0,0 +1,19 @@
+export async function requestAccessToken(accessCode) {
+    return fetch("/auth/access-token", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        accessCode: accessCode,
+      }),
+    });
+  }
+  
+  export async function saveAccessToken(accessToken) {
+    localStorage.setItem("ACCESS_TOKEN", accessToken);
+  }
+  
+  export async function getAccessToken() {
+    return localStorage.getItem("ACCESS_TOKEN");
+  }
